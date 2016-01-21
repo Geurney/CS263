3efcc45eebd6370f6e33d86a73ed0367875da0b9
Geurney <dongqiao.ma@gmail.com>
2016-01-20 16:43:11 -0800

    datastore/memcache step 4

--- base_project/base/src/main/java/cs263w16/DatastoreServlet.java
+++ base_project/base/src/main/java/cs263w16/DatastoreServlet.java
@@ -13,7 +13,15 @@ public class DatastoreServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+      MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
+      syncCache.setErrorHandler(ErrorHandlers.getConsistentLogAndContinue(Level.INFO));   
+
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+      String keyname = null;
+      String value = null;
+      Date date = null;
+
       resp.setContentType("text/html");
       resp.getWriter().println("<html><body>");
 
@@ -21,33 +29,52 @@ public class DatastoreServlet extends HttpServlet {
       if (map.size() == 0) {
           Query q = new Query("TaskData");
 	  PreparedQuery pq = datastore.prepare(q);
-          resp.getWriter().println("<ul>");
+	  ArrayList<String> keynames = new ArrayList<String>();
+
+          resp.getWriter().println("<ul>In DataStore");
 	  for (Entity result : pq.asIterable()) {
-	      String value = (String) result.getProperty("value");
-	      Date date = (Date) result.getProperty("date");
-              resp.getWriter().println("<li> " + value + " " + date + " </li>");
+	      Key key  = result.getKey();
+	      keynames.add(key.getName());
+	      value = (String) result.getProperty("value");
+	      date = (Date) result.getProperty("date");
+              resp.getWriter().println("<li>" + key + "  " + value + " " + date + " </li>");
           }
+          resp.getWriter().println("</ul>");
+
+          resp.getWriter().println("<ul>In MemCache");
+	  for (String key_name : keynames) {
+              value = (String)syncCache.get(key_name);
+              if (value != null) {
+                  resp.getWriter().println("<li>" + key_name + " " + value + " </li>");
+	      }
+	  }
           resp.getWriter().println("</ul>");
       } else {
           if (map.containsKey("keyname")) {
-              String keyname = map.get("keyname")[0];
+              keyname = map.get("keyname")[0];
 	      if (map.containsKey("value")) {
-                  String value = map.get("value")[0];
+                  value = map.get("value")[0];
                   Entity taskData = new Entity("TaskData", keyname);
 		  taskData.setProperty("value", value);
 		  taskData.setProperty("date", new Date());
 		  datastore.put(taskData);
+	          syncCache.put(keyname, value);
                   resp.getWriter().println("<h2>Stored " + keyname + " and " + value + " in Datastore</h2>");
 	      } else {
-	          Key key = KeyFactory.createKey("TaskData", keyname);
-                  try { 
-		      Entity taskData = datastore.get(key);
-	              String value = (String)taskData.getProperty("value");
-		      Date date = (Date)taskData.getProperty("date");
-                      resp.getWriter().println("<h2>" + value + " " + date + " </h2>");
-	          } catch (EntityNotFoundException e) {
-                      resp.getWriter().println("<h2>Entity not found</h2>");
-	          }
+                  value = (String) syncCache.get(keyname);
+                  if (value == null) {
+	              Key key = KeyFactory.createKey("TaskData", keyname);
+                      try { 
+		          Entity taskData = datastore.get(key);
+	                  value = (String)taskData.getProperty("value");
+	                  syncCache.put(keyname, value);
+			  resp.getWriter().println("<h2>" + keyname + ": " + value + " (Datastore)</h2>");
+   	              } catch (EntityNotFoundException e) {
+                          resp.getWriter().println("<h2>Neither found</h2>");
+	              }
+		  } else {
+                      resp.getWriter().println("<h2>" + keyname + ": " + value + " (Both)</h2>");
+		  }
 	      }
 	  } else {
               resp.getWriter().println("<h2>Wrong parameters</h2>");
